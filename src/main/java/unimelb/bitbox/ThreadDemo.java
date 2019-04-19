e718ea2065fc82d265abc9a9c403f9a5e78ed6a7
Juncheng-Li <Juncheng.alex.li@gmail.com>
2019-04-19 16:32:25 +1000

    transfer

--- src/main/java/unimelb/bitbox/ThreadDemo.java
+++ src/main/java/unimelb/bitbox/ThreadDemo.java
@@ -5,47 +5,47 @@ public class ThreadDemo extends Thread
     private Thread t;
     private String threadName;
 
-    public ThreadDemo (String name)
-    {
-        threadName = name;
-        System.out.println("Creating " + threadName + "thread");
-    }
 
     public void run()
     {
-        System.out.println("Running " + threadName + "thread");
+        //System.out.println("Running " + threadName + "thread");
+
+        for (int i = 44; i >= 0; i--)
+        {
+            System.out.println("Thread print" + "," + i);
+            try
+            {
+                Thread.sleep(1000);
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(" exiting...");
+    }
+
+
+
+
+
+    public static void main(String[] args)
+    {
+        ThreadDemo iron = new ThreadDemo();
+        ThreadDemo cap = new ThreadDemo();
+
+        iron.start();
 
         try
         {
-            for (int i = 4; i >= 0; i--)
-            {
-                System.out.println("Thread " + threadName + "," + i);
-                Thread.sleep(2000);
-            }
+            iron.join();
+
         } catch (InterruptedException e)
         {
             e.printStackTrace();
         }
 
-        System.out.println(threadName + " exiting...");
-    }
-
-    public void start()
-    {
-        System.out.println(threadName + "Start running -----");
-        if (t == null)
-        {
-            t = new Thread(this, threadName);
-            t.start();
-        }
-    }
-
-    public static void main(String[] args)
-    {
-        ThreadDemo iron = new ThreadDemo("Iron man");
-        ThreadDemo cap = new ThreadDemo("Cap");
-
-        iron.start();
         cap.start();
+
     }
 }
