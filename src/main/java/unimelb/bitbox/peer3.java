90b0e11a6473e1fcfa7fa08e94289b34d5880657
Juncheng-Li <Juncheng.alex.li@gmail.com>
2019-04-26 12:23:07 +1000

    multi-threading client

---
+++ src/main/java/unimelb/bitbox/peer3.java
@@ -0,0 +1,29 @@
+package unimelb.bitbox;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import unimelb.bitbox.util.Configuration;
+import unimelb.bitbox.util.FileSystemManager;
+
+import javax.net.ServerSocketFactory;
+import java.net.*;
+import java.util.Scanner;
+
+public class peer3
+{
+    public static void main(String[] args)
+    {
+        Client T1 = new Client("peer3", "localhost", 3000);
+
+
+        T1.start();
+    }
+
+}
