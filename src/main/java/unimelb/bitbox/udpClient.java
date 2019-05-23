e0660e97271d3915211b1d8b731a6b699260b390
Juncheng-Li <Juncheng.alex.li@gmail.com>
2019-05-23 13:39:14 +1000

    test udp

--- src/main/java/unimelb/bitbox/udpClient.java
+++ src/main/java/unimelb/bitbox/udpClient.java
@@ -3,16 +3,29 @@ package unimelb.bitbox;
 // Java program to illustrate Client side
 // Implementation using DatagramSocket
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import unimelb.bitbox.util.Configuration;
 
+import javax.swing.text.html.parser.Parser;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
+import java.util.Timer;
 
 public class udpClient extends Thread
 {
+    private JSONParser parser = new JSONParser();
+    private ServerMain f;
+    private Timer timer = new Timer();
+
+    udpClient()
+    {
+
+    }
+
     public void run()
     {
 
@@ -22,7 +35,7 @@ public class udpClient extends Thread
             // carrying the data.
             DatagramSocket dsSocket = new DatagramSocket();  //SocketException
             InetAddress ip = InetAddress.getByName("10.0.0.79"); //UnknownHostException
-            int port = Integer.parseInt(Configuration.getConfigurationValue("udpPort"));
+            int udpPort = Integer.parseInt(Configuration.getConfigurationValue("udpPort"));
             byte buf[] = null;
 
             // Handshake - fixed!
@@ -33,7 +46,7 @@ public class udpClient extends Thread
             hs.put("command", "HANDSHAKE_REQUEST");
             hs.put("hostPort", hostPort);
             buf = hs.toJSONString().getBytes();
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, ip, port);
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, ip, udpPort);
             dsSocket.send(packet);  //IOException  //need "/n" ?
             System.out.println("udp sent: " + hs);
 
@@ -59,43 +72,69 @@ public class udpClient extends Thread
             }
              */
 
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
 
         //ServerSide
-        try
-        {
+
             // Step 1 : Create a socket to listen at port 1234
-            int udpPort = Integer.parseInt(Configuration.getConfigurationValue("udpPort"));
-            DatagramSocket dsSocket = new DatagramSocket(udpPort); //
+            //int udpPort = Integer.parseInt(Configuration.getConfigurationValue("udpPort"));
+            DatagramSocket dsServerSocket = new DatagramSocket(udpPort); //
             byte[] receive = new byte[65535];
             System.out.println("UDP port " + udpPort);
 
-            DatagramPacket packet = null;
+            DatagramPacket serverPacket = null;
             while (true)
             {
+                // Step 2 : create a DatagramPacket to receive the data.
+                serverPacket = new DatagramPacket(receive, receive.length);
 
-                // Step 2 : create a DatgramPacket to receive the data.
-                packet = new DatagramPacket(receive, receive.length);
+                // Step 3 : receive the data in byte buffer.
+                dsServerSocket.receive(serverPacket);  //
+                StringBuilder message = data(receive);
+                System.out.println("udpClient: " + message);
 
-                // Step 3 : revieve the data in byte buffer.
-                dsSocket.receive(packet);  //
-                System.out.println(packet.getAddress());
-                System.out.println(packet.getSocketAddress());
-                System.out.println(packet.getPort());
-                System.out.println(packet.getData());
-                System.out.println(packet.getLength());
-                System.out.println("udpClient: " + data(receive));
+                /*
+                // Client side, copied from server
+                JSONObject command = (JSONObject) parser.parse(message.toString());
+                System.out.println("Message from UDP peer: " + command.toJSONString());
+
+                if (command.getClass().getName().equals("org.json.simple.JSONObject"))
+                {
+                    if (command.get("command").toString().equals("HANDSHAKE_RESPONSE"))
+                    {
+                        // Synchronizing Events after Handshake!!!
+                        timer.schedule(new SyncEvents(f), 0,
+                                Integer.parseInt(Configuration.getConfigurationValue("syncInterval")) * 1000);
+                    } else if (command.get("command").toString().equals("CONNECTION_REFUSED"))
+                    {
+                        System.out.println("Peer(" +
+                                socket.getRemoteSocketAddress().toString().replaceAll("/", "")
+                                + ") maximum connection limit reached...");
+                        break;
+                    } else
+                    {
+                        commNProcess process_T = new commNProcess(command, socket, f);
+                        process_T.start();
+                    }
+                } else
+                {
+                    // If not a JSONObject
+                    JSONObject reply = new JSONObject();
+                    reply.put("command", "INVALID_PROTOCOL");
+                    reply.put("message", "message must contain a command field as string");
+                    System.out.println("sent: " + reply);
+                    out.write(reply + "\n");
+                    out.flush();
+                }
+                 */
 
                 // Exit the server if the client sends "bye"
+                /*
                 if (data(receive).toString().equals("bye"))
                 {
                     System.out.println("udpClient sent bye.....EXITING");
                     break;
                 }
+                 */
 
                 // Clear the buffer after every message.
                 receive = new byte[65535];
